f2b593ee39d4587f155e352fdf941a9d252e8616
kianaptran <kianaptran@g.ucla.edu>
2024-07-01 16:11:25 -0700

    finished week 1 lab

--- week 1 profile/js/init.js
+++ week 1 profile/js/init.js
@@ -1,7 +1,7 @@
 // Initialize the map
 const map = new maplibregl.Map({
     container: 'map', // container ID
-    style: 'https://api.maptiler.com/maps/streets/style.json?key=wsyYBQjqRwKnNsZrtci1', // Your style URL
+    style: 'https://api.maptiler.com/maps/dataviz/style.json?key=3o1p8QOuX7nNtoM3YRz0', // Your style URL
     center: [ -118.44925, 34.07213], // Starting position [lng, lat]
     zoom: 15 // Starting zoom level
 });
@@ -10,17 +10,17 @@ const map = new maplibregl.Map({
 new maplibregl.Marker()
     .setLngLat([ -118.44585, 34.07196])
     .setPopup(new maplibregl.Popup({ offset: 25 }) // Add popups
-        .setHTML('John Wooden Center: catch me trying to sneak in a quick pump'))
+        .setHTML('<div class="pink_background">John Wooden Center: catch me trying to sneak in a quick pump'))
     .addTo(map);
 
     new maplibregl.Marker()
     .setLngLat([ -118.45348, 34.07278])
     .setPopup(new maplibregl.Popup({ offset: 25 }) // Add popups
-        .setHTML('Olympic Study Hall: usually here studying or napping'))
+        .setHTML('<div class="pink_background">Olympic Study Hall: usually here studying or napping'))
     .addTo(map);
 
     new maplibregl.Marker()
     .setLngLat([ -118.44456, 34.07058])
     .setPopup(new maplibregl.Popup({ offset: 25 }) // Add popups
-        .setHTML('UCLA Store Ackerman: my place of work'))
+        .setHTML('<div class="pink_background">UCLA Store Ackerman: my place of work'))
     .addTo(map);
